dcd4bc7d0d024dec9b7f61700c02b65ccdbb6c06
Meliza Caguimbal <melizacaguimbal34@gmail.com>
2020-08-06 00:12:10 +0800

    add city name and country in the weather list row

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -1,19 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class GoogleMap extends Component {
-	componentDidMount() {
-		new google.maps.Map(this.refs.map, {
-			zoom: 12,
-			center: {
-				lat: this.props.lat,
-				lng: this.props.lon
-			}
-		});
-	}
+  componentDidMount() {
+    new google.maps.Map(this.refs.map, {
+      zoom: 12,
+      center: {
+        lat: this.props.lat,
+        lng: this.props.lon,
+      },
+    });
+  }
 
-	render() {
-		return <div ref="map" />;
-	}
+  render() {
+    const { name, country } = this.props;
+    return (
+      <div>
+        <div>{`${name}, ${country}`}</div>
+        <div ref="map" />
+      </div>
+    );
+  }
 }
 
 export default GoogleMap;
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -5,7 +5,7 @@ import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
 	renderWeather(cityData) {
-		const name = cityData.city.name;
+		const { name, country } = cityData.city;
 		const temps = cityData.list.map(weather => weather.main.temp);
 		const pressures = cityData.list.map(weather => weather.main.pressure);
 		const humidities = cityData.list.map(weather => weather.main.humidity);
@@ -13,7 +13,7 @@ class WeatherList extends Component {
 
 		return  (
 			<tr key={name}>
-				<td><GoogleMap lon={lon} lat={lat} /></td>
+				<td><GoogleMap lon={lon} lat={lat} name={name} country={country} /></td>
 				<td><Chart data={temps} color="orange" units="K" /></td>
 				<td><Chart data={pressures} color="green" units="hPa" /></td>
 				<td><Chart data={humidities} color="black" units="%" /></td>
